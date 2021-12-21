569fedb1e380cf7455f1c19c688e7bdd83439c4c
Marcel Roesink <RSK01@rocvantwente.nl>
2021-12-21 10:20:24 +0100

    Add project files.

---
+++ Opdrachten_C_Sharp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Opdrachten_C_Sharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
