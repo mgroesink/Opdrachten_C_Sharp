c2b3a2f52750b5051c3030d6f148ad133e29e43d
Marcel Roesink <RSK01@rocvantwente.nl>
2022-02-01 13:10:29 +0100

    Eind les 1-2-2022

---
+++ Opdrachten_C_Sharp/Controllers/Opdracht3Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Opdrachten_C_Sharp.Controllers
+{
+    public class Opdracht3Controller : Controller
+    {
+        public IActionResult Opdracht3_11()
+        {
+            /*
+             * Maak een programma met als naam Vandaag dat informatie geeft 
+             * over de huidige dag. De volgende informatie moet worden getoond:
+                • Het huidige jaar
+                • De huidige maand (als woord)
+                • De huidige dag (als woord)
+                • De hoeveelste dag van de maand het is
+                • Het huidige weeknummer
+                • De hoeveelste dag van het jaar het is
+            */
+            var today = DateTime.Today;
+            int year = today.Year;
+            var monthWord = today.ToString("MMMM");
+            ViewBag.Year = year;
+            ViewBag.Month = monthWord;
+
+
+            return View();
+        }
+    }
+}
