aa59b0e704eefceb50ca4e81c545b067f27ee939
Marcel Roesink <RSK01@rocvantwente.nl>
2022-02-11 16:12:53 +0100

    Opdracht 4.1 Opdracht 4.2

--- Opdrachten_C_Sharp/Controllers/Opdracht4Controller.cs
+++ Opdrachten_C_Sharp/Controllers/Opdracht4Controller.cs
@@ -6,6 +6,60 @@ namespace Opdrachten_C_Sharp.Controllers
 {
     public class Opdracht4Controller : Controller
     {
+        public IActionResult Opdracht4_1(double? getal1 = null, double? getal2 = null)
+        {
+            /*
+             * Lees twee getallen in en geef aan welk van beide getallen 
+             * het grootste is. 
+             * Als beide getallen hetzelfde zijn dient dat ook weergegeven 
+             * te worden. De uitvoer is dus één van de volgende:
+                • Het eerste getal is het grootste
+                • Het tweede getal is het grootste
+                • De getallen zijn gelijk aan elkaar
+             */
+            if (getal1 != null && getal2 != null)
+            {
+                if (getal1 == getal2)
+                {
+                    ViewBag.Result = "De getallen zijn gelijk";
+                }
+                else if (getal1 < getal2)
+                    ViewBag.Result = "Getal 2 is het grootst";
+                else
+                    ViewBag.Result = "Getal 1 is het grootst";
+                ViewBag.getal1 = getal1;
+                ViewBag.getal2 = getal2;
+            }
+            return View();
+        }
+
+        public IActionResult Opdracht4_2(double? getal1 = null, 
+            double? getal2 = null, double? getal3 = null)
+        {
+            /*
+             Schrijf een programma dat 3 getallen inleest. 
+             Als de eerste twee beide groter zijn dan het derde 
+             dient als uitvoer getoond te worden:
+                Het derde getal is het kleinste van de drie.
+                Als dat niet zo is moet als tekst verschijnen:
+                Het derde getal is niet het kleinste van de drie.
+             */
+            if (getal1 != null && getal2 != null && getal3 != null)
+            {
+                if (getal3 < getal2 && getal3 < getal1)
+                {
+                    ViewBag.Result = "Het derde getal is het kleinste van de drie";
+                }
+                else
+                {
+                    ViewBag.Result = "Het derde getal is niet het kleinste van de drie";
+                }
+                ViewBag.getal1 = getal1;
+                ViewBag.getal2 = getal2;
+                ViewBag.getal3 = getal3;
+            }
+            return View();
+        }
         public IActionResult Opdracht4_16()
         {
             /*
