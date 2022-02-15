767dd9bc13d8fa3b9653176fa27f29c927c8df42
mgroesink <mroesink@rocvantwente.nl>
2022-02-15 16:40:23 +0100

    Merge conficts

---
+++ Opdrachten_C_Sharp/Controllers/StaticMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace Opdrachten_C_Sharp.Controllers
+{
+    public static class StaticMethods
+    {
+        public static string Encrypt(string password, int key)
+        {
+            password = password.ToUpper();
+            StringBuilder sb = new StringBuilder();
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            foreach (char c in password)
+            {
+                int pos = alphabet.IndexOf(c);
+                int newPos = (pos + key) % 26;
+                char newChar = alphabet[newPos];
+                sb.Append(newChar);
+            }
+
+            return sb.ToString();
+        }
+
+        public static string Decrypt(string encrypted, int key)
+        {
+            encrypted = encrypted.ToUpper();
+            StringBuilder sb = new StringBuilder();
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            foreach (char c in encrypted)
+            {
+                int pos = alphabet.IndexOf(c);
+                int newPos = (pos - key + 26) % 26;
+                char newChar = alphabet[newPos];
+                sb.Append(newChar);
+            }
+
+            return sb.ToString();
+        }
+
+        public static string FilterCharacters(string filter)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower() + "0123456789";
+
+            foreach (var letter in filter)
+            {
+                alphabet = alphabet.Replace(letter.ToString(), "");
+            }
+            return alphabet;
+        }
+    }
+}
---
+++ Opdrachten_C_SharpTests1/Controllers/StaticMethodsTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opdrachten_C_Sharp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdrachten_C_Sharp.Controllers.Tests
+{
+    [TestClass()]
+    public class StaticMethodsTests
+    {
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            string password = "HALLO";
+            Assert.AreEqual("KDOOR", StaticMethods.Encrypt(password, 3));
+            password = "MAZZEL";
+            Assert.AreEqual("PDCCHO", StaticMethods.Encrypt(password, 3));
+        }
+
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            string password = "KDOOR";
+            Assert.AreEqual("HALLO", StaticMethods.Decrypt(password, 3));
+            password = "PDCCHO";
+            Assert.AreEqual("MAZZEL", StaticMethods.Decrypt(password, 3));
+        }
+
+        [TestMethod()]
+        public void FilterCharactersTest()
+        {
+            string expected = "ABCDEGHIJKLMNOPQRSTUVWXYZ" +
+                "ABCDFGHIJKLMNOPQRSTUVWXYZ".ToLower() + "012346789";
+            string actual = StaticMethods.FilterCharacters("Fe5");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
